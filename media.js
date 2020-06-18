a1bd049e9a1f49abe5d04619057f0d6b0f902494
Ana Buzzi <66036121+anabuzzi@users.noreply.github.com>
2020-06-18 15:59:57 -0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,21 @@
+const aluno01 = 'Fulano1'
+const notaAluno01 = 9.8
+
+const aluno02 = 'Fulano2'
+const notaAluno02 = 10
+
+const aluno03 = 'Fulano3'
+const notaAluno03 = 2
+
+
+
+const media = (notaAluno01 + notaAluno02 +notaAluno03) / 3 
+   
+    if (media > 5) {
+
+    console.log (`A nota foi de ${media}. Parabéns!`) 
+
+}else{
+    console.log (`A média é menor que 5. Você foi reprovado!`) 
+    
+    }
